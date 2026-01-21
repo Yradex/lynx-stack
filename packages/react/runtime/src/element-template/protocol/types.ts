fdba559a6c30b2898bde5aec050d596772175724
yradex <11014207+Yradex@users.noreply.github.com>
2026-01-21 14:04:58 +0800

    refactor: comment out unused properties in `ElementTemplateFlushOptions` interface.

--- packages/react/runtime/src/element-template/protocol/types.ts
+++ packages/react/runtime/src/element-template/protocol/types.ts
@@ -12,21 +12,21 @@ export type SerializedETInstance = [
 export type ElementTemplatePatchStream = (number | string | null | unknown[])[];
 
 export interface ElementTemplateFlushOptions {
-  triggerLayout?: boolean;
-  operationID?: any;
-  __lynx_timing_flag?: string;
-  nativeUpdateDataOrder?: number;
-  elementID?: number;
-  listID?: number;
-  listReuseNotification?: {
-    listElement: FiberElement;
-    itemKey: string;
-  };
+  // triggerLayout?: boolean;
+  // operationID?: any;
+  // __lynx_timing_flag?: string;
+  // nativeUpdateDataOrder?: number;
+  // elementID?: number;
+  // listID?: number;
+  // listReuseNotification?: {
+  //   listElement: FiberElement;
+  //   itemKey: string;
+  // };
   pipelineOptions?: PipelineOptions;
-  elementIDs?: number[];
-  operationIDs?: any[];
-  asyncFlush?: boolean;
-  triggerDataUpdated?: boolean;
+  // elementIDs?: number[];
+  // operationIDs?: any[];
+  // asyncFlush?: boolean;
+  // triggerDataUpdated?: boolean;
 }
 
 export interface ElementTemplateCommitContext {
